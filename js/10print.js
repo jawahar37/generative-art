b8cbfe6d8b1e898c30c2d1dd424823bc9cf40cd5
Jawahar Pinnelli <j.pinnelli@sap.com>
2020-10-15 16:36:51 +0530

    Scope functions and variables properly.

--- js/10print.js
+++ js/10print.js
@@ -1,60 +1,66 @@
-var canvas = document.getElementById('10print');
-var context = canvas.getContext('2d');
+_10print = function() {
+	var canvas = document.getElementById('10print');
+	var context = canvas.getContext('2d');
 
 
-if(window.innerWidth > 600) {
-	var width = 600;
-	var height = 500;
-}
-else {
-	var width = 400;
-	var height = 500;
-}
+	if(window.innerWidth > 600) {
+		var width = 600;
+		var height = 500;
+	}
+	else {
+		var width = 400;
+		var height = 500;
+	}
 
-canvas.style.width = width + "px";
-canvas.style.height = height + "px";
+	canvas.style.width = width + "px";
+	canvas.style.height = height + "px";
 
-var pixelScale = Math.floor(window.devicePixelRatio);
-canvas.width = width * pixelScale;
-canvas.height = height * pixelScale;
-context.scale(pixelScale, pixelScale)
+	var pixelScale = Math.floor(window.devicePixelRatio);
+	canvas.width = width * pixelScale;
+	canvas.height = height * pixelScale;
+	context.scale(pixelScale, pixelScale)
 
 
-_10print();
+	draw();
 
-function _10print() {
-	context.clearRect(0, 0, width, height);
-	drawMaze(25, 4);
-}
+	function draw() {
+		context.clearRect(0, 0, width, height);
+		drawMaze(25, 4);
+	}
 
-function drawMaze(step, slashWidth) {
-	context.lineCap = "square";
-	context.lineWidth = slashWidth;
-	context.strokeStyle = "white";
+	function drawMaze(step, slashWidth) {
+		context.lineCap = "square";
+		context.lineWidth = slashWidth;
+		context.strokeStyle = "white";
 
-	context.beginPath();
-	for(var x = 0; x < width; x += step) {
-		for(var y = 0; y < height; y+= step) {
-			drawSlash(x, y, step, step);    
+		context.beginPath();
+		for(var x = 0; x < width; x += step) {
+			for(var y = 0; y < height; y+= step) {
+				drawSlash(x, y, step, step);    
+			}
+		}
+		context.stroke();
+	}
+
+	function drawSlash(x, y, width, height) {
+		var leftToRight = probability(0.5);
+
+		if(leftToRight) {
+			context.moveTo(x, y);
+			context.lineTo(x + width, y + height);    
+		} else {
+			context.moveTo(x + width, y);
+			context.lineTo(x, y + height);
 		}
 	}
-	context.stroke();
-}
 
-function drawSlash(x, y, width, height) {
-	var leftToRight = probability(0.5);
-
-	if(leftToRight) {
-		context.moveTo(x, y);
-		context.lineTo(x + width, y + height);    
-	} else {
-		context.moveTo(x + width, y);
-		context.lineTo(x, y + height);
+	function probability(probability) {
+		if(probability > 1 || probability <= 0)
+			return 0;
+		return Math.random() >= probability;
 	}
-}
 
-function probability(probability) {
-	if(probability > 1 || probability <= 0)
-		return 0;
-	return Math.random() >= probability;
-}
+	return  {
+		draw:draw
+	}
+}();
--- js/spiro.js
+++ js/spiro.js
@@ -1,70 +1,76 @@
-var canvas = document.getElementById('spiro');
-var ctx = canvas.getContext('2d');
+spiro = function() {
+	var canvas = document.getElementById('spiro');
+	var ctx = canvas.getContext('2d');
 
 
-if(window.innerWidth > 600) {
-	var width = 600;
-	var height = 600;
-}
-else {
-	var width = 400;
-	var height = 400;
-}
-
-canvas.style.width = width + "px";
-canvas.style.height = height + "px";
-
-var pixelScale = Math.floor(window.devicePixelRatio);
-canvas.width = width * pixelScale;
-canvas.height = height * pixelScale;
-ctx.scale(pixelScale, pixelScale)
-
-document.getElementById("spiro-radius").value = 0.667;
-document.getElementById("spiro-loops").value = 4;
-var phase = 0;
-spiro();
-
-function spiro() {
-	ctx.clearRect(0, 0, width, height);
-	ctx.lineWidth = 7;
-
-	let totalRadius = width * 3/8;
-	let radiusRatio = document.getElementById("spiro-radius").value;
-	let innerRadius = totalRadius * radiusRatio, outerRadius = totalRadius * (1 - radiusRatio);
-	
-	let rotationRatio = document.getElementById("spiro-loops").value;
-	
-	drawSpiro(width/2, height/2, innerRadius, outerRadius, 1.0/rotationRatio, rotationRatio);
-	phase += 0.01;
-}
-
-function drawSpiro(centerX, centerY, innerRadius, outerRadius, ratio, loops) {
-
-	let lastX = centerX + innerRadius + outerRadius * Math.cos(phase);
-	let lastY = centerY + outerRadius * Math.sin(phase);
-
-	for (let theta = 0; theta < (loops * 2 * Math.PI) + 0.01; theta += 0.01) {
-		let x =
-			centerX +
-			innerRadius * Math.cos(theta) +
-			outerRadius * Math.cos(theta * ratio + phase);
-		let y =
-			centerY +
-			innerRadius * Math.sin(theta) +
-			outerRadius * Math.sin(theta * ratio + phase);
-		
-		let color = "hsl(" + Math.floor((theta - phase) * 360 / (2 * Math.PI)) + ", 100%, 50%)";
-
-		drawLine(lastX, lastY, x, y, color);
-		lastX = x;
-		lastY = y;
+	if(window.innerWidth > 600) {
+		var width = 600;
+		var height = 600;
 	}
-}
+	else {
+		var width = 400;
+		var height = 400;
+	}
 
-function drawLine(x1, y1, x2, y2, color) {
-	ctx.beginPath();
-	ctx.moveTo(x1, y1);
-	ctx.lineTo(x2, y2);
-	ctx.strokeStyle = color;
-	ctx.stroke();
-}
+	canvas.style.width = width + "px";
+	canvas.style.height = height + "px";
+
+	var pixelScale = Math.floor(window.devicePixelRatio);
+	canvas.width = width * pixelScale;
+	canvas.height = height * pixelScale;
+	ctx.scale(pixelScale, pixelScale)
+
+	document.getElementById("spiro-radius").value = 0.667;
+	document.getElementById("spiro-loops").value = 4;
+	var phase = 0;
+	draw();
+
+	function draw() {
+		ctx.clearRect(0, 0, width, height);
+		ctx.lineWidth = 7;
+
+		let totalRadius = width * 3/8;
+		let radiusRatio = document.getElementById("spiro-radius").value;
+		let innerRadius = totalRadius * radiusRatio, outerRadius = totalRadius * (1 - radiusRatio);
+		
+		let rotationRatio = document.getElementById("spiro-loops").value;
+		
+		drawSpiro(width/2, height/2, innerRadius, outerRadius, 1.0/rotationRatio, rotationRatio);
+		phase += 0.01;
+	}
+
+	function drawSpiro(centerX, centerY, innerRadius, outerRadius, ratio, loops) {
+
+		let lastX = centerX + innerRadius + outerRadius * Math.cos(phase);
+		let lastY = centerY + outerRadius * Math.sin(phase);
+
+		for (let theta = 0; theta < (loops * 2 * Math.PI) + 0.01; theta += 0.01) {
+			let x =
+				centerX +
+				innerRadius * Math.cos(theta) +
+				outerRadius * Math.cos(theta * ratio + phase);
+			let y =
+				centerY +
+				innerRadius * Math.sin(theta) +
+				outerRadius * Math.sin(theta * ratio + phase);
+			
+			let color = "hsl(" + Math.floor((theta - phase) * 360 / (2 * Math.PI)) + ", 100%, 50%)";
+
+			drawLine(lastX, lastY, x, y, color);
+			lastX = x;
+			lastY = y;
+		}
+	}
+
+	function drawLine(x1, y1, x2, y2, color) {
+		ctx.beginPath();
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+		ctx.strokeStyle = color;
+		ctx.stroke();
+	}
+
+	return  {
+		draw:draw
+	}
+}();
