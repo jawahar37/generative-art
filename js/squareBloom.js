ab3c12b88b2675003d6d922478d5c2f92f8ad261
Jawahar Pinnelli <j.pinnelli@sap.com>
2020-10-17 17:56:30 +0530

    Optimize Square Bloom.

--- js/squareBloom.js
+++ js/squareBloom.js
@@ -20,28 +20,38 @@ squareBloom = function() {
 	canvas.height = height * pixelScale;
 	ctx.scale(pixelScale, pixelScale)
 
+
+
 	//parameters
-	var borderWidth = width<600? 3 : 5,
-	padding = width<600? 6 : 10,
-	count = 150,
-	threshold = 8;
-	maximumAttempts = 300;
+	var borderWidth = 1,
+	padding = 3,
+	threshold = 2,
+	count = 10000,
+	maximumAttempts = 100;
 
 	var pallete = ["#ff595e","#ffca3a","#8ac926","#1982c4", "#6A4C93"];	
+	ctx.lineWidth = borderWidth;
 
 	function Square(x, y, size) {
 		this.x = x;
 		this.y = y;
 		this.size = size;
+		this.children = [];
 	}
 
-	Square.prototype.manhattanEdgeDistance = function(x, y) {
-		var centerDistance = Math.max(Math.abs(this.x - x), Math.abs(this.y - y));
-		return Math.abs(centerDistance - this.size);
+	Square.prototype.getManhattanCenterDistance = function(x, y) {
+		return Math.max(Math.abs(this.x - x), Math.abs(this.y - y));
 	}
 
-	Square.prototype.drawPath = function() {
+	Square.prototype.isPointEnclosed = function(x, y) {
+		return this.getManhattanCenterDistance(x, y) <= this.size;
+	}
+
+	Square.prototype.draw = function() {
+		ctx.strokeStyle = pallete[rangeFloor(0, pallete.length)]
+		ctx.beginPath();
 		ctx.rect(this.x - this.size, this.y - this.size, this.size * 2, this.size * 2)
+		ctx.stroke();
 	}
 
 	draw();
@@ -49,58 +59,71 @@ squareBloom = function() {
 	function draw() {
 		ctx.clearRect(0, 0, width, height);
 
-		squares = [];
-		squares.push(new Square(rangeFloor(0, width), rangeFloor(0, height), rangeFloor(20, 60)));
-
-		while(squares.length < count) {	//generate new squares
-			var square = attemptNewSquare(maximumAttempts);
-			if(square == null)
-				break;
-			
-			squares.push(square);
-		}
-
-		ctx.lineWidth = borderWidth;
-		ctx.strokeStyle = "white";
-		for (var i = 0; i < squares.length; i++) {
-			ctx.strokeStyle = pallete[rangeFloor(0, pallete.length)]
-			ctx.beginPath();
-			squares[i].drawPath();
-			ctx.stroke();
-		}
+		var root = new Square(width/2, height/2, (width + padding)/2);
+		// root.children.push(new Square(rangeFloor(0, width), rangeFloor(0, height), rangeFloor(20, 60)));
+		generateSquares(root);
+		drawSquares(root);
 	}
 
-	function attemptNewSquare(attempts) {
-		var x = rangeFloor(0, width);
-		var y = rangeFloor(0, height);
-		var size = getFeasibleSize(x, y);
-
-		if(size > threshold) {	//filters squares that are too small
-			return new Square(x, y, size);
-		}
-		else if(attempts-- <= 0) {	//stop the process if a valid position for a new square can't be found even after many attempts.
-			return null;
-		}
-		else
-			return attemptNewSquare(attempts);
+	function generateSquares(root) {
+		var squaresGenerated = 0;
+		var fails = 0;
+		while(squaresGenerated < count) {	//generate new squares till either enough squares are drawn are till attempts expire
+			var attemptsLeft = maximumAttempts;
 		
+			while(--attemptsLeft > 0) {
+				var x = rangeFloor(0, width);
+				var y = rangeFloor(0, height);
+				var enclosingRoot = getEnclosingSquare(x, y, root);
+				var size = getFeasibleSize(x, y, enclosingRoot);
+
+				if(size > threshold) {	//filters squares that are too small
+					var square = new Square(x, y, size);
+					enclosingRoot.children.push(square);
+					squaresGenerated++;
+					break;
+				}
+			}
+			fails += maximumAttempts - attemptsLeft;
+			if(attemptsLeft <= 0)	//halt generation of squares if attempts run out
+				break;
+		}
+		console.log(squaresGenerated, fails);
 	}
 
-	function getFeasibleSize(x, y) {
-		var potentialSize = width; //intialize potential size to the largest value it can take.
+	function getEnclosingSquare(x, y, root) {
+		var enclosingRoot = root;
 
-		squares.forEach(function(square) {	//find the closest square
-			potentialSize = Math.min(square.manhattanEdgeDistance(x, y), potentialSize);
-		});
+		if(root.children.length > 0) {
+			root.children.forEach(function(square) {
+				if(square.isPointEnclosed(x, y)) {
+					enclosingRoot = getEnclosingSquare(x, y, square);
+				}
+			});
+		}
+		return enclosingRoot;	//return root if the point is outside all children
+	}
+
+	function getFeasibleSize(x, y, root) {
+		var potentialSize = root.size; //intialize potential size to the largest value it can take.
+
+			potentialSize = Math.min(root.size - root.getManhattanCenterDistance(x, y), potentialSize);
+
+			root.children.forEach(function(square) {	//find the closest square
+				potentialSize = Math.min(square.getManhattanCenterDistance(x, y) - square.size, potentialSize);
+			});
 		return potentialSize - padding;
 	}
 
+	function drawSquares(root) {
+		root.draw();
+		root.children.forEach(drawSquares);
+	}
 
 	function rangeFloor(min, max) {
 		// Return a random whole number between min and max
 		return Math.floor(Math.random() * (max - min) + min);
 	}
-
 
 	return  {
 		draw:draw
